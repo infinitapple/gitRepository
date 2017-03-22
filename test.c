7cffab7ccaef07fc960ffd9ad584e5faf89a4198
YOURIEPI <dltkdals329@naver.com>
2017-03-22 10:46:21 +0900

    .

--- test.c
+++ test.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
 
 int main(void) {
-<<<<<<< HEAD
-	int i = 111312;
-=======
-	int i = 23456433;
->>>>>>> e4664ebabb73bb00b134c695210b8206da90dcc0
-
+	int i = 1;
+	// modified
 	printf("%d\n", i);
 
 	return 0;
